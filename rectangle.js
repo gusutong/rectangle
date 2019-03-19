61a98493c715a87510b977fa6ece887574f605db
gusutong <562656798@qq.com>
2019-03-19 16:45:56 +0800

    rectangle --v0.3

---
+++ rectangle.js
@@ -0,0 +1,17 @@
+/* global rectangle:true */
+$(function(){
+  var width = $('#width'),
+      height = $('#height'),
+      btnCal = $('#calculate'),
+      perimeter = $('#perimeter'),
+      area = $('#area');
+  btnCal.click(function(){
+
+    var w = Number(width.val()),
+        h = Number(height.val());
+
+    var rect = rectangle();
+    perimeter.val(rect.perimeter(w,h));
+    area.val(rect.area(w,h));                  
+    });
+});
